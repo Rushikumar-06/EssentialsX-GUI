265f647d61a63f419417ae239ffb53ec7ca9f883
Sniper_TVmc <snipertv59.pro@gmail.com>
2025-01-19 12:39:34 +0100

    [⚡] Update EssentialsXGUI command to use it in console

--- src/main/java/fr/snipertvmc/essentialsxgui/commands/CommandEssentialsXGUI.java
+++ src/main/java/fr/snipertvmc/essentialsxgui/commands/CommandEssentialsXGUI.java
@@ -4,7 +4,6 @@ import fr.snipertvmc.essentialsxgui.Main;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 public class CommandEssentialsXGUI implements CommandExecutor {
 
@@ -15,20 +14,15 @@ public class CommandEssentialsXGUI implements CommandExecutor {
 	@Override
     public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
 
-    	if (!(commandSender instanceof Player player)) {
-    		commandSender.sendMessage("§cVous ne pouvez pas utiliser cette commande dans la console.");
-    		return true;
-    	}
-
-		player.sendMessage("");
-		player.sendMessage("  §6EssentialsX-GUI §7- §fPlugin by §eSniper_TVmc");
-		player.sendMessage("    §8■ §7§oEssentialsX-GUI is an addon for EssentialsX that adds some GUIs to the plugin.");
-		player.sendMessage("");
-		player.sendMessage("    §8■ §7Version: §b" + Main.getInstance().getDescription().getVersion());
-		player.sendMessage("    §8■ §7Discord: §3discord.gg/§k8ZzQ6Q5");
-		player.sendMessage("    §8■ §7Spigot: §6spigotmc.org/resources/§k100000");
-		player.sendMessage("    §8■ §7GitHub: §fgithub.com/SniperTVmc/EssentialsX-GUI");
-		player.sendMessage("");
+		commandSender.sendMessage("");
+		commandSender.sendMessage("  §6EssentialsX-GUI §7- §fPlugin by §eSniper_TVmc");
+		commandSender.sendMessage("    §8■ §7§oEssentialsX-GUI is an addon for EssentialsX that adds some GUIs to the plugin.");
+		commandSender.sendMessage("");
+		commandSender.sendMessage("    §8■ §7Version: §b" + Main.getInstance().getDescription().getVersion());
+		commandSender.sendMessage("    §8■ §7Discord: §3discord.gg/§k8ZzQ6Q5");
+		commandSender.sendMessage("    §8■ §7Spigot: §6spigotmc.org/resources/§k100000");
+		commandSender.sendMessage("    §8■ §7GitHub: §fgithub.com/SniperTVmc/EssentialsX-GUI");
+		commandSender.sendMessage("");
 
     	return true;
     }
