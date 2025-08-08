ebbe63609f5f2743eba4c9c1106e0af903748c02
Sniper_TVmc <snipertv59.pro@gmail.com>
2025-08-08 12:03:34 +0200

    [✨] Added TypeUtils static class to manage more easily types

---
+++ src/main/java/fr/snipertvmc/essentialsxgui/utilities/data/TypeUtils.java
@@ -0,0 +1,84 @@
+package fr.snipertvmc.essentialsxgui.utilities.data;
+
+public class TypeUtils {
+
+
+	// -------------------------------------------------- //
+
+
+	public static boolean isByte(String value) {
+		try {
+			Byte.parseByte(value);
+			return true;
+
+		} catch (NumberFormatException e) {
+			return false;
+		}
+	}
+
+
+	public static boolean isShort(String value) {
+		try {
+			Short.parseShort(value);
+			return true;
+
+		} catch (NumberFormatException e) {
+			return false;
+		}
+	}
+
+
+	public static boolean isInteger(String value) {
+		try {
+			Integer.parseInt(value);
+			return true;
+
+		} catch (NumberFormatException e) {
+			return false;
+		}
+	}
+
+
+	public static boolean isLong(String value) {
+		try {
+			Long.parseLong(value);
+			return true;
+
+		} catch (NumberFormatException e) {
+			return false;
+		}
+	}
+
+
+	public static boolean isFloat(String value) {
+		try {
+			Float.parseFloat(value);
+			return true;
+
+		} catch (NumberFormatException e) {
+			return false;
+		}
+	}
+
+
+	public static boolean isDouble(String value) {
+		try {
+			Double.parseDouble(value);
+			return true;
+
+		} catch (NumberFormatException e) {
+			return false;
+		}
+	}
+
+
+	// -------------------------------------------------- //
+
+
+	public static boolean isBoolean(String value) {
+		return value.equalsIgnoreCase("true") || value.equalsIgnoreCase("false");
+	}
+
+
+	// -------------------------------------------------- //
+}
